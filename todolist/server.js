7471426233b98b8a036eb51b038a41db9b8d23e5
Nila <Nila@LAPTOP-M0QERO78>
2021-03-23 18:33:33 -0400

    done

--- todolist/server.js
+++ todolist/server.js
@@ -1,7 +1,11 @@
+const { render } = require('ejs');
 var express = require('express');
 var app = express()
 app.set('view engine','ejs')
 let tasks=[]
+let id=0
+
+
 app.get('/',(req,res)=>{
     res.render('index',{tasks})
   })
@@ -11,16 +15,16 @@ app.get('/newtask',(req,res)=>{
 app.use(express.urlencoded({
     extended:true
 }))
-let id=0
+
 app.post('/',(req,res)=>{
     let record={id:id, title:req.body.title,due:req.body.due, description:req.body.description}
     tasks.push(record)
     id++
-    console.log(tasks)
+    //console.log(tasks)
     res.redirect('/')
 })
-app.delete('/',(req,res)=>{
-    console.log(req)
+app.get('/:id',(req,res)=>{
+    res.render('View')
 })
 
 app.listen(3000, function () {
--- todolist/views/Data.js
+++
@@ -1,9 +0,0 @@
-function changeText(){
-    let button=document.getElementsByClassName("bools")
-    let i=0
-    for(i=0; i<button.length; i++){
-        button[i].id=i
-        console.log(button[i].id)
-    }
-}
-changeText()
